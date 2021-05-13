4704cb159e300a5f56c3989f8412dfe3f62f023f
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-05-13 07:02:11 -0300

    "Life" system for entities added

---
+++ Assets/Scritps/Game/EntityLife.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityLife : MonoBehaviour, IHitable
+{
+    [SerializeField] int lifes;
+
+    public void TakeDamage(int _damage){
+        lifes -= _damage;
+    }
+}
