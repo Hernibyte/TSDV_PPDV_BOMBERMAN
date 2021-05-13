0d3fafc0b647b023d69b0dce63ded5244835427b
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-05-13 06:39:37 -0300

    Player movement funtion class added

---
+++ Assets/Scritps/Game/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float movementVelocity;
+    float xAxis;
+    float zAxis;
+    float smooth;
+    Vector3 newPosition;
+
+    void Update()
+    {
+        UpdateAxis();
+        UpdateNewPosition();
+        PlayerMove();
+    }
+
+    void UpdateAxis()
+    {
+        xAxis = Input.GetAxis("Horizontal");
+        zAxis = Input.GetAxis("Vertical");
+    }
+
+    void UpdateNewPosition()
+    {
+        smooth = Time.deltaTime * movementVelocity;
+        newPosition = new Vector3(xAxis * smooth, 0f, zAxis * smooth);
+    }
+
+        void PlayerMove()
+    {
+        transform.Translate(newPosition);
+    }
+}
