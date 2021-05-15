1121c021536f0b3c64b4c383b8d9a53a95c45086
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-05-15 03:23:20 -0300

    Game manager and IHitable interface added

---
+++ Assets/Scritps/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IHitable{
+    void TakeDamage(int _damage);
+}
+
+public class GameManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
